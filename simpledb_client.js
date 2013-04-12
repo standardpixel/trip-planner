cff65f2977d4962a830663a8933ad1140e9d5ca7
Eric Gelinas <eric@standardpixel.com>
2013-04-11 19:57:18 -0700

    clean up

--- simpledb_client.js
+++
@@ -1,5 +0,0 @@
-var AWS  = require('aws-sdk'),
-	keys = require(__dirname + '/keys.json'),
-	sdb  = aws.createSimpleDBClient(keys.aws.key, keys.aws.secret, {secure:false});
-	
-exports.client = sdb;
